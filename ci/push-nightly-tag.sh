724976c8315ce98e77b3eba2d61826fc2595eaba
Max Risuhin <risuhin.max@gmail.com>
2019-02-12 20:59:27 +0200

    Update nightly build tag generation script
    Signed-off-by: Max Risuhin <risuhin.max@gmail.com>

--- ci/push-nightly-tag.sh
+++ ci/push-nightly-tag.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Starting deploy script..."
+echo "Starting nightly build deploy script..."
 MY_TAG="$( git describe --exact-match "$(git rev-parse HEAD)" 2>/dev/null )"
 if [ -z "$MY_TAG" ] ; then
-  echo "Variable is empty, giong to try to push a tag..."
+  echo "Tag for last commit is not found, going to try to push new nighlty build tag..."
 
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
@@ -11,10 +11,10 @@ if [ -z "$MY_TAG" ] ; then
   NEW_TAG="Nightly-$(date +%Y-%m-%d)-$(git rev-parse --short HEAD)"
   git tag -a $NEW_TAG -m "Nightly Build Tag $NEW_TAG"
 
-  echo "New generated tag: $NEW_TAG"
+  echo "New generated nightly build tag: $NEW_TAG"
 
   git remote add origin-repo https://${GH_TOKEN}@github.com/MaxRis/aminal.git > /dev/null 2>&1
   git push origin-repo $NEW_TAG
 else
-  echo "MY_TAG=$MY_TAG"
+  echo "Skipping nighly build tag generation. Last commit tag found:$MY_TAG"
 fi
